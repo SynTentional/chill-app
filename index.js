b832d586f16be657720e9531c6f350b78094500e
Christopher Mullins <cmullins318@gmail.com>
2021-07-01 20:20:50 -0700

    merissaaaa

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const exphbs = require('express-handlebars');
+const app = express()
+const port = 3000
+
+// MIDDLEWEAR
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'))
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+  
+  // EXPORTS APP FOR MOCHA TESTING
+  module.exports = app;
