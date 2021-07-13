456a7bb339a2ebe1477d238876a64fa4a814b6b6
Christopher Mullins <cmullins318@gmail.com>
2021-07-13 13:11:24 -0700

    fixed more html styling

--- models/store.js
+++ models/store.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose')
 
 const storeSchema = new Schema({
     name: { type: String, required: true },
@@ -7,5 +8,10 @@ const storeSchema = new Schema({
     // Display # of people in Queue
     queue: { type: Number, required: true}
 });
+
+// Example models
+
+const Target = mongoose.model('Target', storeSchema);
+const Best_Buy = mongoose.model('Best Buy', storeSchema);
 
 module.exports = model('Store', storeSchema);
--- routes/index.js
+++ routes/index.js
@@ -14,12 +14,15 @@ module.exports = (app) => {
         // Run route
         // Populate page with store data
         // Stores.find({}).lean.populate('stores')
-            //.then((index) => res.render('index', { stores, user }))
+        //     .then((index) => res.render('index', { stores, user }))
         res.render('index')
 
     });
 
+
     // TODO: Display list of stores
+
+    // Populate Mongoose models with a list of sample stores
 
     // WHEN NOT SIGNED IN DISPLAYS SIGN UP BUTTON
 
