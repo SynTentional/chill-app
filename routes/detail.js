b43d2f895ea5eeef0dd4f2b01b4c1a6e7f3d04b9
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2021-07-13 20:23:35 -0700

    random

--- routes/detail.js
+++ routes/detail.js
@@ -12,14 +12,9 @@ module.exports = (app) => {
         res.render('detail')
     })
 
-<<<<<<< HEAD
     app.get('/:store', (req, res) => {
         const store = Store.findById(req.params.id)
         res.render('detail', {store})
-=======
-    app.get('/store/store._id', (req, res) => {
-        const { user } = req;
->>>>>>> d141df1a550710b1cb14a6c4b361b1b9185dc2c7
         // Run route
         console.log(req.cookies);
         res.render('detail')
